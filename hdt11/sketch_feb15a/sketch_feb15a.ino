56dea4f2cfbb5840c67f9874c2296dc47b1f54be
Jaume Arús <jaume.arus@paack.co>
2021-02-28 20:06:10 +0100

    Added hdt11

---
+++ hdt11/sketch_feb15a/sketch_feb15a.ino
@@ -0,0 +1,36 @@
+#include "DHT.h"      // DHT LIBRARY FROM ADAFRUIT
+
+#define DHTPIN 2        // SENSOR PIN
+#define DHTTYPE DHT11   // SENSOR TYPE
+
+DHT dht(DHTPIN, DHTTYPE); // Initialize dht sensor
+
+void setup(){
+  Serial.begin(9600); // Initialize serial console
+
+  dht.begin();        // Initialize DHT sensor
+}
+
+
+void loop(){
+  delay(5000);
+
+  float h = dht.readHumidity(); 
+  float t = dht.readTemperature();
+
+  /*
+  if (isnan(h) || isnan(t)){
+    Serial.println("Some went wrong reading from DHT11");
+    return;
+  }
+  */
+
+  float hic = dht.computeHeatIndex(t,h,false); // Calculate heat index in Centigrads
+
+  Serial.print("Humidity ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatura:  ");
+  Serial.print(t);
+  Serial.println(" °C");
+}
